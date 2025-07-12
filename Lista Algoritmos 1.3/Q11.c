17ff43cbe4680ed36cb2e9a85c03bd84339b3c90
Arthur Barbosa <arthursantosbarbosa123@gmail.com>
2025-07-11 21:00:54 -0300

    Update Q11.c

--- Lista Algoritmos 1.3/Q11.c
+++ Lista Algoritmos 1.3/Q11.c
@@ -2,16 +2,14 @@
 
 int main() {
 	int mes, ano;
+	int bissexto = 0;
 	printf("Digite o mes (1-12): ");
 	scanf("%d", &mes);
 	printf("Digite o ano: ");
 	scanf("%d", &ano);
-	if (ano % 4 == 0 || ano % 400 == 0) {
-		ano = 366;
-	}
-	else {
-		ano = 365;
-	}
+	if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+        	bissexto = 1;
+    	}
 	switch (mes) {
 				case 1: case 3: case 5: case 7: case 8: case 10: case 12:
 			printf("31 dias\n");
@@ -30,6 +28,6 @@ int main() {
 			printf("Mes invalido\n");
 			break;
 	}
-	printf("Total de dias no ano: %d\n", ano);
+	printf("Total de dias no ano: %d\n", bissexto ? 366 : 365);
 	return 0;
 }
